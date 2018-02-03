e7921a6afea24af492464c383224df2f201c396a
Homer Huang <aclisp@gmail.com>
2018-02-03 11:41:32 +0800

    Fix my Auth token.

--- logic/auth.go
+++ logic/auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"goim/libs/define"
 	"strconv"
+	log "github.com/thinkboy/log4go"
 )
 
 // developer could implement "Auth" interface for decide how get userId, or roomId
@@ -18,12 +19,20 @@ func NewDefaultAuther() *DefaultAuther {
 }
 
 func (a *DefaultAuther) Auth(token string) (userId int64, roomId int32) {
+	log.Info("Auth token is %s", token)
 	var err error
-	if userId, err = strconv.ParseInt(token, 10, 64); err != nil {
-		userId = 0
-		roomId = define.NoRoom
-	} else {
-		roomId = 1 // only for debug
+	userId = 0
+	roomId = define.NoRoom
+	defer func() {
+		log.Info("Auth userId is %v, roomId is %v", userId, roomId)
+	}()
+	if len(token) < 2 {
+		return
 	}
+	token = token[1 : len(token)-1]
+	if userId, err = strconv.ParseInt(token, 10, 64); err != nil {
+		return
+	}
+	roomId = 1 // only for debug
 	return
 }
